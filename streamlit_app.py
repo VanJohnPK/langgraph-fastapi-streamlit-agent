4680b86a4bb2947b69dd646d8d96bdffb1fef1a0
Van John <1585781755@qq.com>
2024-08-13 15:17:11 +0800

    modify streamlit_app

---
+++ streamlit_app.py
@@ -0,0 +1,238 @@
+import asyncio
+import os
+from typing import AsyncGenerator, List
+
+import streamlit as st
+from streamlit.runtime.scriptrunner import get_script_run_ctx
+from client import AgentClient
+from schema import ChatMessage
+
+
+# A Streamlit app for interacting with the langgraph agent via a simple chat interface.
+# The app has three main functions which are all run async:
+
+# - main() - sets up the streamlit app and high level structure
+# - draw_messages() - draws a set of chat messages - either replaying existing messages
+#   or streaming new ones.
+
+# The app heavily uses AgentClient to interact with the agent's FastAPI endpoints.
+
+
+APP_TITLE = "darkVinci's Agent"
+APP_ICON = "🐹"
+
+@st.cache_resource
+def get_agent_client():
+    agent_url = os.getenv("AGENT_URL", "http://localhost")
+    return AgentClient(agent_url)
+
+
+async def main():
+    st.set_page_config(
+        page_title=APP_TITLE,
+        page_icon=APP_ICON,
+        menu_items={},
+    )
+
+    # Hide the streamlit upper-right chrome
+    st.html(
+        """
+        <style>
+        [data-testid="stStatusWidget"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+            }
+        </style>
+        """,
+    )
+    if st.get_option("client.toolbarMode") != "minimal":
+        st.set_option("client.toolbarMode", "minimal")
+        await asyncio.sleep(0.1)
+        st.rerun()
+
+    models = {
+        "OpenAI GPT-4o-mini (streaming)": "gpt-4o-mini",
+        "llama-3.1-70b on Groq": "llama-3.1-70b",
+    }
+    # Config options
+    with st.sidebar:
+        st.header(f"{APP_ICON} {APP_TITLE}")
+        ""
+        "Full toolkit for running an AI agent service built with LangGraph, FastAPI and Streamlit"
+        with st.popover(":material/settings: Settings", use_container_width=True):
+            m = st.radio("LLM to use", options=models.keys())
+            model = models[m]
+            use_streaming = st.toggle("Stream results", value=True)
+        
+        @st.dialog("Architecture")
+        def architecture_dialog():
+            st.image("https://github.com/JoshuaC215/agent-service-toolkit/blob/main/media/agent_architecture.png?raw=true")
+            "[View full size on Github](https://github.com/JoshuaC215/agent-service-toolkit/blob/main/media/agent_architecture.png)"
+            st.caption("App hosted on [Streamlit Cloud](https://share.streamlit.io/) with FastAPI service running in [Azure](https://learn.microsoft.com/en-us/azure/app-service/)")
+
+        if st.button(":material/schema: Architecture", use_container_width=True):
+            architecture_dialog()
+
+        with st.popover(":material/policy: Privacy", use_container_width=True):
+            st.write("Prompts, responses and feedback in this app are anonymously recorded and saved to LangSmith for product evaluation and improvement purposes only.")
+
+        "[View the source code](https://github.com/JoshuaC215/agent-service-toolkit)"
+        st.caption("Made with :material/favorite: by [Joshua](https://www.linkedin.com/in/joshua-k-carroll/) in Oakland")
+
+    # Draw existing messages
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+    messages: List[ChatMessage] = st.session_state.messages
+
+    if len(messages) == 0:
+        WELCOME = "Hello! I'm an AI-powered research assistant with web search and a calculator. I may take a few seconds to boot up when you send your first message. Ask me anything!"
+        with st.chat_message("ai"):
+            st.write(WELCOME)
+
+    # draw_messages() expects an async iterator over messages
+    async def amessage_iter():
+        for m in messages: yield m
+    await draw_messages(amessage_iter())
+
+    # Generate new message if the user provided new input
+    if input := st.chat_input():
+        messages.append(ChatMessage(type="human", content=input))
+        st.chat_message("human").write(input)
+        agent_client = get_agent_client()
+        if use_streaming:
+            stream = agent_client.astream(
+                message=input,
+                model=model,
+                thread_id=get_script_run_ctx().session_id,
+            )
+            await draw_messages(stream, is_new=True)
+        else:
+            response = await agent_client.ainvoke(
+                message=input,
+                model=model,
+                thread_id=get_script_run_ctx().session_id,
+            )
+            messages.append(response)
+            st.chat_message("ai").write(response.content)
+        st.rerun() # Clear stale containers
+
+
+async def draw_messages(
+        messages_agen: AsyncGenerator[ChatMessage | str, None],
+        is_new=False,
+    ):
+    """
+    Draws a set of chat messages - either replaying existing messages
+    or streaming new ones.
+
+    This function has additional logic to handle streaming tokens and tool calls.
+    - Use a placeholder container to render streaming tokens as they arrive.
+    - Use a status container to render tool calls. Track the tool inputs and outputs
+      and update the status container accordingly.
+    
+    The function also needs to track the last message container in session state
+    since later messages can draw to the same container. This is also used for
+    drawing the feedback widget in the latest chat message.
+
+    Args:
+        messages_aiter: An async iterator over messages to draw.
+        is_new: Whether the messages are new or not.
+    """
+
+    # Keep track of the last message container
+    last_message_type = None
+    st.session_state.last_message = None
+
+    # Placeholder for intermediate streaming tokens
+    streaming_content = ""
+    streaming_placeholder = None
+
+    # Iterate over the messages and draw them
+    while msg := await anext(messages_agen, None):
+        # str message represents an intermediate token being streamed
+        if isinstance(msg, str):
+            # If placeholder is empty, this is the first token of a new message
+            # being streamed. We need to do setup.
+            if not streaming_placeholder:
+                if last_message_type != "ai":
+                    last_message_type = "ai"
+                    st.session_state.last_message = st.chat_message("ai")
+                with st.session_state.last_message:
+                    streaming_placeholder = st.empty()
+            
+            streaming_content += msg
+            streaming_placeholder.write(streaming_content)
+            continue
+        if not isinstance(msg, ChatMessage):
+            st.error(f"Unexpected message type: {type(msg)}")
+            st.write(msg)
+            st.stop()
+        match msg.type:
+            # A message from the user, the easiest case
+            case "human":
+                last_message_type = "human"
+                st.chat_message("human").write(msg.content)
+
+            # A message from the agent is the most complex case, since we need to
+            # handle streaming tokens and tool calls.
+            case "ai":
+                # If we're rendering new messages, store the message in session state
+                if is_new:
+                    st.session_state.messages.append(msg)
+                
+                # If the last message type was not AI, create a new chat message
+                if last_message_type != "ai":
+                    last_message_type = "ai"
+                    st.session_state.last_message = st.chat_message("ai")
+                
+                with st.session_state.last_message:
+                    # If the message has content, write it out.
+                    # Reset the streaming variables to prepare for the next message.
+                    if msg.content:
+                        if streaming_placeholder:
+                            streaming_placeholder.write(msg.content)
+                            streaming_content = ""
+                            streaming_placeholder = None
+                        else:
+                            st.write(msg.content)
+
+                    if msg.tool_calls:
+                        # Create a status container for each tool call and store the
+                        # status container by ID to ensure results are mapped to the
+                        # correct status container.
+                        call_results = {}
+                        for tool_call in msg.tool_calls:
+                            status = st.status(
+                                    f"""Tool Call: {tool_call["name"]}""",
+                                    state="running" if is_new else "complete",
+                                )
+                            call_results[tool_call["id"]] = status
+                            status.write("Input:")
+                            status.write(tool_call["args"])
+
+                        # Expect one ToolMessage for each tool call.
+                        for _ in range(len(call_results)):
+                            tool_result: ChatMessage = await anext(messages_agen)
+                            if not tool_result.type == "tool":
+                                st.error(f"Unexpected ChatMessage type: {tool_result.type}")
+                                st.write(tool_result)
+                                st.stop()
+                            
+                            # Record the message if it's new, and update the correct
+                            # status container with the result
+                            if is_new:
+                                st.session_state.messages.append(tool_result)
+                            status = call_results[tool_result.tool_call_id]
+                            status.write("Output:")
+                            status.write(tool_result.content)
+                            status.update(state="complete")
+
+            # In case of an unexpected message type, log an error and stop
+            case _: 
+                st.error(f"Unexpected ChatMessage type: {msg.type}")
+                st.write(msg)
+                st.stop()
+
+if __name__ == "__main__":
+    asyncio.run(main())
