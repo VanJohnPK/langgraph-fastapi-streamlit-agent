11b59268861add0fb014958bf133181eb5840188
Van John <1585781755@qq.com>
2024-08-14 19:52:12 +0800

    整理tools

--- agent/research_assistant.py
+++ agent/research_assistant.py
@@ -1,21 +1,22 @@
-from datetime import datetime
-from typing import Literal
 from langchain_openai import ChatOpenAI
+
 # from langchain_groq import ChatGroq
 from langchain_core.language_models.chat_models import BaseChatModel
-from langchain_core.messages import AIMessage, SystemMessage, RemoveMessage
+from langchain_core.messages import AIMessage, SystemMessage
 from langchain_core.runnables import RunnableConfig, RunnableLambda
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import END, StateGraph, MessagesState
 from langgraph.managed import IsLastStep
 from langgraph.prebuilt import ToolNode
-from langchain_community.tools.ddg_search.tool import DuckDuckGoSearchResults
-from agent.tools import web_search, arxiv_search, wiki, datetime_tool, calculator
+from agent.tools import web_search, arxiv_search, datetime_tool
 from dotenv import load_dotenv
+
 load_dotenv()
+
 
 class AgentState(MessagesState):
     is_last_step: IsLastStep
+
 
 # NOTE: models with streaming=True will send tokens as they are generated
 # if the /stream endpoint is called with stream_tokens=True (the default)
@@ -23,22 +24,17 @@ class AgentState(MessagesState):
 
 models = {
     "gpt-4o-mini": ChatOpenAI(model="gpt-4o-mini", temperature=0.5, streaming=True),
-    "gpt-4o-2024-08-06": ChatOpenAI(model="gpt-4o-2024-08-06", temperature=0.5, streaming=True),
+    "gpt-4o-2024-08-06": ChatOpenAI(
+        model="gpt-4o-2024-08-06", temperature=0.5, streaming=True
+    ),
 }
-tools = [web_search, arxiv_search, wiki, datetime_tool, calculator]
-current_date = datetime.now().strftime("%B %d, %Y")
+tools = [web_search, arxiv_search, datetime_tool]
 instructions = f"""
     You are a helpful research assistant with the ability to search the web for information.
-    Today's date is {current_date}.
-    
-    NOTE: THE USER CAN'T SEE THE TOOL RESPONSE.
-
-    A few things to remember:
-    - Please include markdown-formatted links to any citations used in your response. Only include one
+    Please include markdown-formatted links to any citations used in your response. Only include one
     or two citations per response unless more are needed. ONLY USE LINKS RETURNED BY THE TOOLS.
-    - Use calculator tool with numexpr to answer math questions. The user does not understand numexpr,
-      so for the final response, use human readable format - e.g. "300 * 200", not "(300 \\times 200)".
     """
+
 
 def wrap_model(model: BaseChatModel):
     model = model.bind_tools(tools)
@@ -47,6 +43,7 @@ def wrap_model(model: BaseChatModel):
         name="StateModifier",
     )
     return preprocessor | model
+
 
 async def acall_model(state: AgentState, config: RunnableConfig):
     m = models[config["configurable"].get("model", "gpt-4o-mini")]
@@ -65,45 +62,15 @@ async def acall_model(state: AgentState, config: RunnableConfig):
     return {"messages": [response]}
 
 
-
-async def block_unsafe_content(state: AgentState, config: RunnableConfig):
-    output_messages = []
-
-    # Remove the last message if it's an AI message
-    last_message = state["messages"][-1]
-    if last_message.type == "ai":
-        output_messages.append(RemoveMessage(id=last_message.id))
-
-    return {"messages": output_messages}
-
 # Define the graph
 agent = StateGraph(AgentState)
 agent.add_node("model", acall_model)
 agent.add_node("tools", ToolNode(tools))
-# agent.add_node("guard_input", llama_guard_input)
-# agent.add_node("block_unsafe_content", block_unsafe_content)
-# agent.set_entry_point("guard_input")
 agent.set_entry_point("model")
-
-# Check for unsafe input and block further processing if found
-# def unsafe_input(state: AgentState):
-#     safety: LlamaGuardOutput = state["safety"]
-#     match safety.safety_assessment:
-#         case "unsafe":
-#             return "block_unsafe_content"
-#         case _:
-#             return "model"
-# agent.add_conditional_edges(
-#     "guard_input",
-#     unsafe_input,
-#     {"block_unsafe_content": "block_unsafe_content", "model": "model"}
-# )
-
-# Always END after blocking unsafe content
-#agent.add_edge("block_unsafe_content", END)
 
 # Always run "model" after "tools"
 agent.add_edge("tools", "model")
+
 
 # After "model", if there are tool calls, run "tools". Otherwise END.
 def pending_tool_calls(state: AgentState):
@@ -112,36 +79,10 @@ def pending_tool_calls(state: AgentState):
         return "tools"
     else:
         return END
+
+
 agent.add_conditional_edges("model", pending_tool_calls, {"tools": "tools", END: END})
 
 research_assistant = agent.compile(
     checkpointer=MemorySaver(),
 )
-
-
-if __name__ == "__main__":
-    import asyncio
-    from uuid import uuid4
-    from dotenv import load_dotenv
-
-    load_dotenv()
-    
-    async def main():
-        inputs = {"messages": [("user", "Find me a recipe for chocolate chip cookies")]}
-        result = await research_assistant.ainvoke(
-            inputs,
-            config=RunnableConfig(configurable={"thread_id": uuid4()}),
-        )
-        result["messages"][-1].pretty_print()
-
-        # Draw the agent graph as png
-        # requires:
-        # brew install graphviz
-        # export CFLAGS="-I $(brew --prefix graphviz)/include"
-        # export LDFLAGS="-L $(brew --prefix graphviz)/lib"
-        # pip install pygraphviz
-        # 
-        # researcH_assistant.get_graph().draw_png("agent_diagram.png")
-
-
-    asyncio.run(main())
--- agent/tools.py
+++ agent/tools.py
@@ -1,53 +1,19 @@
-from langchain_core.tools import Tool, tool, BaseTool
-from langchain_community.tools import DuckDuckGoSearchResults, ArxivQueryRun, WikipediaQueryRun
-from langchain_community.utilities import WikipediaAPIWrapper
+from langchain_core.tools import Tool
+from langchain_community.tools import (
+    DuckDuckGoSearchResults,
+    ArxivQueryRun,
+)
 from datetime import datetime
-from langchain.tools import Tool
-import numexpr
-import math
-import re
+
 web_search = DuckDuckGoSearchResults(name="WebSearch")
 
 # Kinda busted since it doesn't return links
 arxiv_search = ArxivQueryRun(name="ArxivSearch")
 
-wiki = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
+# wiki = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
 
 datetime_tool = Tool(
     name="Datetime",
     func=lambda x: datetime.now().isoformat(),
     description="Returns the current datetime",
 )
-
-def calculator_func(expression: str) -> str:
-    """Calculates a math expression using numexpr.
-    
-    Useful for when you need to answer questions about math using numexpr.
-    This tool is only for math questions and nothing else. Only input
-    math expressions.
-
-    Args:
-        expression (str): A valid numexpr formatted math expression.
-
-    Returns:
-        str: The result of the math expression.
-    """
-
-    try:
-        local_dict = {"pi": math.pi, "e": math.e}
-        output = str(
-            numexpr.evaluate(
-                expression.strip(),
-                global_dict={},  # restrict access to globals
-                local_dict=local_dict,  # add common mathematical functions
-            )
-        )
-        return re.sub(r"^\[|\]$", "", output)
-    except Exception as e:
-        raise ValueError(
-            f'calculator("{expression}") raised error: {e}.'
-            " Please try again with a valid numerical expression"
-        )
-
-calculator: BaseTool = tool(calculator_func)
-calculator.name = "Calculator"
