f3b1487905e3563267f62484d5f28c5c85600bf8
Van John <1585781755@qq.com>
2024-08-16 14:50:49 +0800

    add wikipedia

--- agent/tools.py
+++ agent/tools.py
@@ -2,7 +2,9 @@ from langchain_core.tools import Tool
 from langchain_community.tools import (
     DuckDuckGoSearchResults,
     ArxivQueryRun,
+    WikipediaQueryRun
 )
+from langchain_community.utilities import WikipediaAPIWrapper
 from datetime import datetime
 
 web_search = DuckDuckGoSearchResults(name="WebSearch")
@@ -10,7 +12,7 @@ web_search = DuckDuckGoSearchResults(name="WebSearch")
 # Kinda busted since it doesn't return links
 arxiv_search = ArxivQueryRun(name="ArxivSearch")
 
-# wiki = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
+wiki = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
 
 datetime_tool = Tool(
     name="Datetime",
