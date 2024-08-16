aaf1cbfc4a1e6debe10815c4ad6d955f33adf8dd
Van John <1585781755@qq.com>
2024-08-16 14:56:19 +0800

    update start message

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,7 +72,7 @@ async def main():
     messages: List[ChatMessage] = st.session_state.messages
 
     if len(messages) == 0:
-        WELCOME = "Hello! I'm an AI-powered research assistant with web search and a calculator. I may take a few seconds to boot up when you send your first message. Ask me anything!"
+        WELCOME = "Hello! I'm an AI-powered research assistant with web search. I may take a few seconds to boot up when you send your first message. Ask me anything!"
         with st.chat_message("ai"):
             st.write(WELCOME)
 
